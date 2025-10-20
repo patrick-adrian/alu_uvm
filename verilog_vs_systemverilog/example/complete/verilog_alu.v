8e2e26f19659b160130f3bc067a36d2e71a8e124
ppineda <ppineda@evertz.com>
2025-10-20 11:01:19 -0400

    Add comments

--- verilog_vs_systemverilog/example/complete/verilog_alu.v
+++ verilog_vs_systemverilog/example/complete/verilog_alu.v
@@ -3,8 +3,19 @@
 //====================
 
 // DUT: Basic ALU
+
+//wire: a physical connection only assigned by continuous
+//assignments, like assign
+//doesn't hold value, carries what is driven to it
+
+//reg: a storage element assigned by procedural
+//assignments, like always or inital blocks
+//holds value until explicitly changed
+//= or <=
+
 module alu (
     input  [7:0] a, b,
+    //inputs are implicitly wire
     input  [2:0] op,
     output reg [7:0] result
 );
@@ -24,13 +35,21 @@ endmodule
 // Simple Verilog Testbench
 //--------------------
 module tb_alu;
+
+//since no ports in a testbench, no input or output here
+
     reg [7:0] a, b;
     reg [2:0] op;
     wire [7:0] result;
 
+    //creating testbench instance
     alu dut (.a(a), .b(b), .op(op), .result(result));
 
+    //procedural block
     initial begin
+
+        //monitor is a system task that continously
+        //prints values when any signals change
         $monitor("time=%0t a=%0d b=%0d op=%b result=%0d", $time, a, b, op, result);
         a = 8'd10; b = 8'd3;
 
